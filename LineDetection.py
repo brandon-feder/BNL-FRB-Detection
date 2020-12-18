d89a304279bd2d06cdff0d0279439b587d2bbe99
Brandon Feder <brandon.e.feder@gmail.com>
2020-12-18 08:31:14 -0500

    Diagnols are now saved

--- LineDetection.py
+++ LineDetection.py
@@ -29,8 +29,14 @@ class LineDetection:
                     for i in range(Nf//2)
                 ])                
                 
+                # outdo.append(vert)
+                # outdo.append(vert)
+                
                 outdo.append(vert)
                 outdo.append(diag)
+
+                # outdo.append(diag)
+                # outdo.append(diag)
 
             todo = outdo
 
@@ -50,23 +56,70 @@ class LineDetection:
         for _ in range( int(math.log(self.dim, 2))):
             outdo = []
             for S in todo:
-                Nf = S.shape[0]
+                Nf, Nt = S.shape
 
                 if isFirst:
                     vert = S[0:Nf:2] + S[1:Nf+1:2]
                     self.V.append(vert)
+
+                    diag = np.array ([
+                            S[ 2*i, i : Nt-Nf//2+i-1] + S[ 2*i+1, i+1 : Nt-Nf//2+i] 
+                        for i in range(Nf//2)
+                    ])
+
+                    self.D.append(diag)
+
                 else:
                     vert = self.V[id][0:Nf, offset:self.V[id].shape[1]]
-                    nVert = S[0:Nf:2, len(S)-offset:len(S)] + S[1:Nf+1:2, len(S)-offset:len(S)]
+                    nVert = S[0:Nf:2, Nt-offset:Nt] + S[1:Nf+1:2, Nt-offset:Nt]
                     self.V[id] = np.concatenate((vert, nVert), 1)
 
-                diag = np.array ([
-                        S[2*i,i:-Nf//2+i-1] + S[2*i+1,i+1:-Nf//2+i] 
-                    for i in range(Nf//2)
-                ])                
-                
+                    Dl = self.D[id].shape[1]
+                    l = Dl - offset
+                    
+                    
+                    if l < 0:
+                        self.D[id] = np.array ([
+                            S[ 2*i, i : Nt-Nf//2+i-1] + S[ 2*i+1, i+1 : Nt-Nf//2+i] 
+                            for i in range(Nf//2)
+                        ])
+
+                    else:  
+                        print("================")
+                        # print("    Case 2")
+
+                        diag = self.D[id][0:Nf, offset:l+offset]
+                        nDiag = np.ndarray((Nf//2, offset))
+
+                        print(Nt-Nf//2-1)
+                        for y in range(Nf//2):
+                            print((y, Nt-Nf//2+y-1), end=", ")
+                            realX = 0
+                            for x in range(y+diag.shape[1], Nt-Nf//2+y-1):
+                                print(y, x)
+                                temp = S[y][x] + S[y+1][x+1] 
+                                nDiag[y][realX] = temp
+                                realX += 1
+                        
+                        print()
+
+
+
+
+                        self.D[id] = np.concatenate((diag, nDiag), 1)
+
                 outdo.append(self.V[id])
-                outdo.append(diag)
+                outdo.append(self.D[id])
+
+                # outdo.append(self.V[id])
+                # outdo.append(self.V[id])
+
+                # outdo.append(self.D[id])
+                # outdo.append(self.D[id])
+
+
+
+                # print(id, S.shape, self.V[id].shape, self.D[id].shape)
 
                 id += 1
 
